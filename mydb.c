dbbfffeaa8c90f98ae2e964ea8bba04c19f2ee35
Vyacheslav Popov <waiz.fdc@gmail.com>
2014-10-10 21:48:43 +0400

    first commit

---
+++ mydb.c
@@ -0,0 +1,44 @@
+#include "mydb.h"
+
+
+int db_close(struct DB *db) {
+	db->close(db);
+}
+
+int db_del(const struct DB *db, void *key, size_t key_len) {
+	struct DBT keyt = {
+		.data = key,
+		.size = key_len
+	};
+	return db->del(db, &keyt);
+}
+
+int db_get(const struct DB *db, void *key, size_t key_len, void **val, size_t *val_len) {
+	struct DBT keyt = {
+		.data = key,
+		.size = key_len
+	};
+	struct DBT valt = {0, 0};
+	int rc = db->get(db, &keyt, &valt);
+	*val = valt.data;
+	*val_len = valt.size;
+	return rc;
+}
+
+int db_put(const struct DB *db, void *key, size_t key_len, void *val, size_t val_len) {
+	struct DBT keyt = {
+		.data = key,
+		.size = key_len
+	};
+	struct DBT valt = {
+		.data = val,
+		.size = val_len
+	};
+	return db->put(db, &keyt, &valt);
+}
+
+int get(const struct DB *db, const struct DBT *key, struct DBT *data) {
+	int i =1;
+
+	return 0;
+}
